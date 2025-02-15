c606c47496a3926ede6bb823e3a1dfcc7f825e27
shahed <shahedibrahim7@gmail.com>
2025-02-15 23:17:40 +0300

    Added Task44

---
+++ Task44/Controllers/GreeterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task44.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreeterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string name = null)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return Ok("Hello anonymous");
+            }
+            return Ok("Hello "+name);
+        }
+    }
+}

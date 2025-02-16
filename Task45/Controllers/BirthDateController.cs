9dbee32fbdd86a71e669986f0163b1af80946b41
shahed <shahedibrahim7@gmail.com>
2025-02-17 02:18:22 +0300

    Added Task45

---
+++ Task45/Controllers/BirthDateController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task45.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BirthDateController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAge([FromQuery] string name = "anonymous", [FromQuery] int? year = null, 
+            [FromQuery] int? month = null, [FromQuery] int? day = null)
+        {
+            if (year == null || month == null || day == null)
+            {
+                return Ok("Hello " + name + ", I can’t calculate your age without knowing your birthdate!");
+            }
+            try
+            {
+                var birthDate = new DateTime(year.Value, month.Value, day.Value);
+                var today = DateTime.Today;
+                var age = today.Year - birthDate.Year;
+                if (birthDate > today.AddYears(-age))
+                {
+                    age--;
+                }
+                return Ok("Hello " + name + ", your age is " + age);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return BadRequest("Hello " + name + ", you entered an invalid birthdate.");
+            }
+        }
+    }
+}
